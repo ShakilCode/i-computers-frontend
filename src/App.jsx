2ed2085e01444b14ed97f1eaaeeeec5717c67c82
ShakilCode <shakilahamedya0108@gmail.com>
2026-04-05 16:50:21 +0530

    Created google login and forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import LoginPage from "./pages/login";
 import Test from "./components/test";
 import { Toaster } from "react-hot-toast";
 import RegisterPage from "./pages/register";
-
+import ForgetPassword from "./pages/forgetPassword";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 
 export default function App() {
 
   return (
+    <GoogleOAuthProvider clientId="836391590494-vnjnfhfv47ef7b52phuhl6lr7j42s7qa.apps.googleusercontent.com">
     <div className="w-full h-screen bg-primary text-secondary">
       <Toaster position="top-right"/>
       <Routes>
@@ -18,9 +20,11 @@ export default function App() {
         <Route path="/admin/*" element={<AdminPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgetPassword />} />
          <Route path="/test" element={<Test />} />
       </Routes>
     </div>
+    </GoogleOAuthProvider>
   )
   
 } 
