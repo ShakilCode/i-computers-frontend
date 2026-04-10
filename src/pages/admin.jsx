9c1df463afd1aa39aaa1f02fcdb406e6a46b0322
ShakilCode <shakilahamedya0108@gmail.com>
2026-04-10 14:27:37 +0530

    Created block user, change role, search product

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -6,6 +6,7 @@ import AdminProductsPage from "./admin/adminProductsPage";
 import AdminAddProductPage from "./admin/adminAddProductPage";
 import AdminUpdateProductPage from "./admin/adminUpdateProductPage";
 import AdminOrdersPage from "./admin/adminOrdersPage";
+import AdminUsersPage from "./admin/adminUsersPage";
 
 export default function AdminPage(){
     return(
@@ -20,7 +21,7 @@ export default function AdminPage(){
                 <Routes>
                     <Route path="/" element={<AdminOrdersPage />} />
                     <Route path="/products" element={<AdminProductsPage />} />
-                    <Route path="/users" element={<h1>Customers Page</h1>} />
+                    <Route path="/users" element={<AdminUsersPage />} />
                     <Route path="/add-product" element={<AdminAddProductPage />}></Route>
                     <Route path="/update-product" element={<AdminUpdateProductPage />}></Route>
                 </Routes>
--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -8,12 +8,17 @@ export default function ProductPage(){
 
     const [products,setProducts] = useState([])
     const [loading,setLoading] = useState(true)
+    const [searchQuery , setSearchQuery] = useState("")
 
     useEffect(
         ()=>{
 
             if(loading){
-                axios.get(import.meta.env.VITE_API_URL + "/products")
+                let url = import.meta.env.VITE_API_URL + "/products/"
+                if(searchQuery!=""){
+                    url = import.meta.env.VITE_API_URL + "/products/search/" + searchQuery
+                }
+                axios.get(url)
                 .then(
                     (response)=>{
                         setProducts(response.data)
@@ -33,11 +38,32 @@ export default function ProductPage(){
 
 
     return(
-        <div className="flex justify-center flex-wrap bg-primary ">
+        <div className="flex justify-center flex-wrap bg-primary relative pt-[80px]">
             {
                 loading && <LoadingAnimation/>
             }
+            <div className="w-full h-[60px] backdrop-blur-sm fixed top-[100px] z-99 flex justify-center items-center">
+                <input type="text" placeholder="Search for products..." className="w-[400px] h-[40px] rounded-full px-4"
+                onChange={
+                    (e)=>{
+                        setSearchQuery(e.target.value)
+                        setLoading(true)
+                    }
+                }/>
+                {/* get all products button */}
+                <button className="ml-4 px-4 py-2 rounded-full bg-secondary text-white"
+                onClick={
+                    ()=>{
+                        setSearchQuery("")
+                        setLoading(true)
+                    }
+                }
+                >
+                    Get all products
+                </button>
+            </div>
             {
+                
                 products.map(
                     (item)=>{
                         return(
